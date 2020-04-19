c4ffcc7f7a2b4415bf8707e7ae9649f6e04a0b0c
rizaljanuari <rizaljanuari4@gmail.com>
2020-04-19 22:33:26 +0700

    Commit Pertama

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:expenses_planner/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses Planner App',
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "tx1",
+      amount: 350000,
+      title: "Sepatu Rebook",
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "tx2",
+      amount: 200000,
+      title: "Belanja Bulanan",
+      date: DateTime.now(),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Expenses Planner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.yellowAccent,
+              child: Text('Chart'),
+            ),
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Text(tx.title),
+            );
+          }).toList())
+        ],
+      ),
+    );
+  }
+}
