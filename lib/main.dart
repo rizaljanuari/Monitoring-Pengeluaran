ad6b6646f7152f3819325b82d7a4c6dc7abbbbdc
rizaljanuari <rizaljanuari4@gmail.com>
2020-04-27 07:41:52 +0700

    Adding images asset, and ternary for the images and list

--- lib/main.dart
+++ lib/main.dart
@@ -29,24 +29,24 @@ class MyHomepage extends StatefulWidget {
 
 class _MyHomepageState extends State<MyHomepage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: "tx1",
-      amount: 350000,
-      title: "Sepatu Rebook",
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "tx2",
-      amount: 200000,
-      title: "Belanja Bulanan",
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "tx3",
-      amount: 8000,
-      title: "Beli Thai Tea",
-      date: DateTime.now(),
-    )
+    // Transaction(
+    //   id: "tx1",
+    //   amount: 350000,
+    //   title: "Sepatu Rebook",
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "tx2",
+    //   amount: 200000,
+    //   title: "Belanja Bulanan",
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: "tx3",
+    //   amount: 8000,
+    //   title: "Beli Thai Tea",
+    //   date: DateTime.now(),
+    // )
   ];
 
   void _addNewTransaction(String title, double amount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,59 +11,76 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2.0, color: Colors.black),
-                  ),
-                  child: SizedBox(
-                    width: 80,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Text("Rp."),
-                        Spacer(),
-                        Text(
-                          //using string interpolation, so i can have a string from number type without adding toSring() function
-                          '${transactions[index].amount.toStringAsFixed(0)}',
-                          // tx.amount.toString(),  <= example if i choose to use without string interpolation
-                        ),
-                      ],
-                    ),
-                  ),
-                  padding: EdgeInsets.all(6),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Center(
+            child: Container(
+                width: 150,
+                child: Column(
                   children: <Widget>[
-                    Text(
-                      transactions[index].title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                    Image.asset(
+                      'assets/images/empty_cart.png',
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      DateFormat.yMMMMEEEEd().format(transactions[index].date),
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'Overpass',
-                        color: Colors.grey,
-                      ),
-                    ),
+                    Text("Belum ada pengeluaran")
                   ],
                 ),
-              ],
+              ),
+          )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 2.0, color: Colors.black),
+                        ),
+                        child: SizedBox(
+                          width: 80,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text("Rp."),
+                              Spacer(),
+                              Text(
+                                //using string interpolation, so i can have a string from number type without adding toSring() function
+                                '${transactions[index].amount.toStringAsFixed(0)}',
+                                // tx.amount.toString(),  <= example if i choose to use without string interpolation
+                              ),
+                            ],
+                          ),
+                        ),
+                        padding: EdgeInsets.all(6),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transactions[index].title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMMEEEEd()
+                                .format(transactions[index].date),
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontFamily: 'Overpass',
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
